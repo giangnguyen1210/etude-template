6ce64f23901476f3d880626b6ff4a0ba4a16b18b
hong.nt1 <giangnguyen.011210@gmail.com>
2024-05-03 15:57:29 +0700

    handle event

--- script.js
+++ script.js
@@ -12,20 +12,41 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-$(document).ready(function () {
-  $(window).scroll(function () {
-    var scrollHeight = 735;
-    var mainColor = '#11171CF2';
-    // Kiểm tra nếu cuộn xuống dưới màn hình
-    if ($(this).scrollTop() > scrollHeight) {
-      // Thay đổi màu nền của header thành đen
-      $('header').css('background-color', mainColor);
-    } else {
-      // Nếu không, header sẽ có màu nền trong suốt
-      $('header').css('background-color', 'transparent');
-    }
-  });
-});
+
+let mybutton = document.getElementById("scrollToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// $(document).ready(function () {
+//   $(window).scroll(function () {
+//     var scrollHeight = 735;
+//     var mainColor = '#11171CF2';
+//     // Kiểm tra nếu cuộn xuống dưới màn hình
+//     if ($(this).scrollTop() > scrollHeight) {
+//       // Thay đổi màu nền của header thành đen
+//       $('header').css('background-color', mainColor);
+//     } else {
+//       // Nếu không, header sẽ có màu nền trong suốt
+//       $('header').css('background-color', 'transparent');
+//     }
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
   // Load JSON data
   fetch('product.json')
@@ -104,3 +125,51 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .catch(error => console.error('Error loading JSON:', error));
 });
+
+var menuItems = document.querySelectorAll('.navbar__menu > li > a');
+console.log(menuItems);
+// Loop through each menu item
+menuItems.forEach(function (item) {
+  // Add click event listener
+  item.addEventListener('click', function () {
+    // Remove 'active' class from all menu items
+    menuItems.forEach(function (item) {
+      item.classList.remove('active');
+    });
+    // Add 'active' class to the clicked menu item
+    this.classList.add('active');
+  });
+});
+
+
+const highlightActiveSection = () => {
+  let sections = document.querySelectorAll('#home, #journey, #gallery, #product, #fashion, #contact');
+
+  console.log(sections);
+  let navLinks = document.querySelectorAll('.navbar__menu > li > a');
+  let top = window.scrollY;
+
+  sections.forEach(sec => {
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+      });
+      document.querySelector('.navbar__menu > li > a[href="#' + id + '"]').classList.add('active');
+    }
+  });
+}
+
+window.onscroll = function () {
+  if (window.scrollY > 100) {
+    document.querySelector(".header").classList.add("scroll");
+  } else {
+    document.querySelector(".header").classList.remove("scroll");
+  }
+
+  highlightActiveSection();
+
+};
